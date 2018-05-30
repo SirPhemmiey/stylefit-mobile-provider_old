f33689f3a619f93deb34d3bc634104ef893750bc
sirphemmiey <oluwafemiakinde@gmail.com>
2018-05-30 12:08:59 -0700

    configure stuffs

--- android/app/src/main/java/com/provider_stylefit/MainActivity.java
+++ android/app/src/main/java/com/provider_stylefit/MainActivity.java
@@ -2,7 +2,6 @@ package com.provider_stylefit;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
-import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,10 +10,6 @@ public class MainActivity extends ReactActivity {
      * This is used to schedule rendering of the component.
      */
     @Override
-     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
-        super.onCreate(savedInstanceState);
-    }
     protected String getMainComponentName() {
         return "provider_stylefit";
     }
