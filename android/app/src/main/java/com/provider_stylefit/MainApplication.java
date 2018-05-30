42cb1699f6b588338a60f1c49d8aa0833a9e8631
sirphemmiey <oluwafemiakinde@gmail.com>
2018-05-30 07:12:55 -0700

    configure stuffs

--- android/app/src/main/java/com/provider_stylefit/MainApplication.java
+++ android/app/src/main/java/com/provider_stylefit/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.airbnb.android.react.maps.MapsPackage;
-import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new MapsPackage(),
-            new SplashScreenReactPackage()
       );
     }
 
